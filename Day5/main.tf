5ab833f7f6974e79562d672ec1c5ecea3ed234a8
deepa-h-0 <hodeepa82@gmail.com>
2025-02-15 23:50:21 +0530

    Add files via upload

---
+++ Day5/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "ami" {
+  description = "This is AMI for the instance"
+}
+
+variable "instance_type" {
+  description = "value"
+  type = map(string)
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+    "prod" = "t2.xlarge"
+  }
+}
+module "EC2_instances" {
+    source = "./module/EC2-instances"
+    ami = var.ami
+    instance_type = var.instance_type
+  
+}
