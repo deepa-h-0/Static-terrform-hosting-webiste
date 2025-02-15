75f67faec709af4878b085458831471fae860036
deepa-h-0 <hodeepa82@gmail.com>
2025-02-15 09:48:05 +0530

    Add files via upload
    creation of ec2 instances via parameter

---
+++ Day2/module/input.tf
@@ -0,0 +1,24 @@
+variable "ami_value" {
+    description = "providing the ami value"
+    type = string
+    
+}
+
+variable "instance_type_value" {
+    description = "providing the instance type"
+    type = string
+    
+}
+
+variable "key_name_value" {
+    description = "providing the key name"
+    type = string
+   
+  
+}
+variable "subnet_id_value" {
+    description = "subnet_id_value"
+    type = string
+    
+  
+}
---
+++ Day2/module/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value = "ami-053a45fff0a704a47"
+instance_type_value = "t2.micro"
+key_name_value = "mypin1"
+subnet_id_value = "subnet-0f621ced88a03757d"
